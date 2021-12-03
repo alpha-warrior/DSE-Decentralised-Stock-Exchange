74690605c71b35af46243ad599ef3d026943e80a
alphawarrior <akshit2@hotmail.com>
2021-12-03 21:51:36 +0530

    Buy_function_completed

--- contracts/Nifty.sol
+++ contracts/Nifty.sol
@@ -3,11 +3,15 @@ pragma solidity 0.5.16;
 
 contract Nifty {
     struct NewOrder{
+        uint order_id; // represents the order ID
         uint State; //represents id order is open for execution 1, cancelled 0 or completed 2
-        address ExecutorAddress;//represents the address of the individual who posted the order
+        address payable ExecutorAddress;//represents the address of the individual who posted the order
         uint Price; //price individual is willing to give or take for the order
         uint quantity_left; //quantity of stocks left to be sold or bought
     }
+
+    uint number_of_orders; 
+    // number of orders placed 
 
     string private Symbol; 
     // string, denoting the name of the asset exchanged by the smart contract
@@ -34,7 +38,8 @@ contract Nifty {
     {
         Symbol = symbol_sent;
         OwnedStocks[address(this)] = quantity_sent;
-        Sell_orders.push(NewOrder(1,address(this),price_sent,quantity_sent));
+        Sell_orders.push(NewOrder(number_of_orders,1,address(this),price_sent,quantity_sent));
+        number_of_orders++;
     }
 
 
@@ -47,7 +52,7 @@ contract Nifty {
         {
             uint found = 0;
             uint found_id = 0;
-            for(uint j=0; j<Buy_orders.length; i++)
+            for(uint j=0; j<Buy_orders.length; j++)
             {
                 if(Buy_orders[j].State == 1)
                 {
@@ -90,7 +95,7 @@ contract Nifty {
         {
             uint found = 0;
             uint found_id = 0;
-            for(uint j=0; j<Sell_orders.length; i++)
+            for(uint j=0; j<Sell_orders.length; j++)
             {
                 if(Sell_orders[j].State == 1)
                 {
@@ -123,15 +128,56 @@ contract Nifty {
         }
         temp_orders.length = 0;
     }    
-//     // A function to post a buy order for underlying security, function will take all the 
-//     // necesarry inputs to post a buy order as parameter and will return the status of order.
-//     function buy() payable public returns(){
-//         // check if the value of money sent matches with quantity*price
-//         // place the order to array of buy orders
-//         // check if any transaction is possible
-//         // process all the possible transactions
-//         // return if order was executed or not
-//     }
+    // A function to post a buy order for underlying security, function will take all the 
+    // necesarry inputs to post a buy order as parameter and will return the status of order.
+    function buy(uint sent_price, uint sent_qty) payable public
+    {
+        require(msg.value == snet_price*sent_qty,"WRONG, amount of money paid"); // check if the value of money sent matches with quantity*price
+        for(uint i=0;i<Sell_orders.length;i++)
+        {
+            if(Sell_orders[i].Price > sent_price)
+            {
+                break;
+            }
+            else
+            {
+                if(Sell_orders.qty_left <= sent_qty)
+                {
+                    uint temp_qty = Sell_orders.qty_left;
+                    OwnedStocks[address(this)] -= temp_qty;
+                    OwnedStocks[msg.sender] += temp_qty;
+                    Sell_orders[i].ExecutorAddress.transfer(sent_price*temp_qty);
+                    Sell_orders[i].qty_left -= temp_qty;
+                    if(Sell_orders[i].qty_left == 0)
+                    {
+                        Sell_orders[i].State = 2;
+                    }
+                    sent_qty -= temp_qty;
+                }
+                else
+                {
+                    uint temp_qty = sent_qty;
+                    OwnedStocks[address(this)] -= temp_qty;
+                    OwnedStocks[msg.sender] += temp_qty;
+                    Sell_orders[i].ExecutorAddress.transfer(sent_price*temp_qty);
+                    Sell_orders[i].qty_left -= temp_qty;
+                    if(Sell_orders[i].qty_left == 0)
+                    {
+                        Sell_orders[i].State = 2;
+                    }
+                    sent_qty -= temp_qty;
+                }
+            }
+        }
+
+        if(sent_qty !=0)
+        {
+            Buy_orders.push(NewOrder(number_of_orders,1,msg.sender,sent_price,sent_qty));
+            number_of_orders++;
+        }
+        sort_Buy_orders();
+        sort_Sell_orders();
+    }
 
 //     // A function to post a sell order for underlying security, function will take all the 
 //     // necesarry inputs to post a sell order as parameter and will return the status of order.
